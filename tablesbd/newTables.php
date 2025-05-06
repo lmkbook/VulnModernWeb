7f8a870b007591999453ff24a9e9c6c0fdd8bcb0
lmkbook <167584338+lmkbook@users.noreply.github.com>
2025-05-06 00:10:18 -0500

    Add files via upload

---
+++ tablesbd/newTables.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tablas</title>
+</head>
+<body>
+    <div>
+        <div>
+            <h1>
+                VulnMordenWeb
+            </h1>
+        </div>
+        <div>
+            <p>
+                Al hacer clic en el botón <b>Actualizar/Crear Tablas</b>, se crearán o restaurarán las tablas de la base de datos al estado por defecto. Esto incluye:<br>
+                <ul>
+                    <li>guest → Encargada de gestionar los comentarios para ataques XSS.</li><br>
+                    <li>users → Será la tabla encargada de gestionar los usuarios que vienen por defecto para acceder al aplicativo.</li><br>
+                    <li>record → Será la encargada de gestionar los ataques registrados por los usuarios</li><br>
+                </ul>
+            </p>
+            <p>
+                Al hacer clic en este botón, todas las bases de datos se restaurarán a su estado original.<br>
+                Esto significa que se eliminarán todos los datos actuales y se volverán a cargar los datos por defecto del sistema.
+            </p>
+        </div>
+        <div>
+            <button value="Enviar">Actualizar/Crear Tablas</button> <button value="Volver">Volver</button>
+        </div>
+    </div>
+</body>
+</html>
