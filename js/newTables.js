d30307751927dff69444ccd44d956c592a9ced05
lmkbook <167584338+lmkbook@users.noreply.github.com>
2025-05-07 17:08:10 -0500

    Add files via upload

---
+++ js/newTables.js
@@ -0,0 +1,32 @@
+document.getElementById('upnewtables').addEventListener("click", function(e){
+    fetch('../model/upnewtable.php', {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(rpt => {
+        if(!rpt.ok){
+            return window.alert(`La respuesta no es un json`);
+        }
+        switch(rpt.status){
+            case 500:
+                return window.alert(`Internal error server`);
+            default:
+                console.log(`Estado: ${rpt.status}`);
+        }
+        return rpt.json();
+    })
+    .then(datos => {
+        if(datos.succes){
+            return alert(datos.succes);
+        }
+        if(datos.fail){
+            return alert(datos.fail);
+        }
+    })
+    .catch(err => {
+        return window.alert(`Error desconocido ${err}`)
+    })
+    e.preventDefault()
+})
